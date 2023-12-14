322f361663286b385d0d2b2d595bbde5fb11c633
nezaking <nezaking@gmail.com>
2023-12-14 18:42:16 +0800

    成功提交到飞书了

--- app/src/main/java/com/xpbiomed/xppda/InboundActivity.java
+++ app/src/main/java/com/xpbiomed/xppda/InboundActivity.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -34,21 +35,23 @@ import java.util.regex.Pattern;
 import java.io.*;
 
 public class InboundActivity extends AppCompatActivity {
-    private static String user="张三";
+    private static String operator = "张三";
     private EditText productionOrderEditText;
     private EditText barcodeEditText;
     private TextView currentQuantityTextView;
     private TextView remainingQuantityTextView;
     private TextView totalQuantityTextView;
     private ListView barCodeListView;
-    private static String feishuAuthor = "Bearer u-eCGP_gsTd59r8AX7VEiu2AghkYHhh4VFiy001gW0aKfl";
+    private static String feishuAuthor = "Bearer u-fRRQ9_MhJbIpVVeELgs7hHghmmHhh4X1ji0040K0aKu4";
     //添加数据适配器
     private ArrayAdapter<String> adapter;
     private List<String> barCodeList;
+    private static List<String> barUrlList; //完整的URL列表
+    Integer listCount = 0;
 
 
-//    private static final String POST_URL = "https://open.feishu.cn/open-apis/bitable/v1/apps/I1RlbBcy8aJEjZsIdalckZFdnse/tables/tblIRpAqIbqfqOCs/records";
-    private static final String POST_URL = "https://open.feishu.cn/open-apis/bitable/v1/apps/I1RlbBcy8aJEjZsIdalckZFdnse/tables/tblIRpAqIbqfqOCs/records";
+    //    private static final String POST_URL = "https://open.feishu.cn/open-apis/bitable/v1/apps/I1RlbBcy8aJEjZsIdalckZFdnse/tables/tblIRpAqIbqfqOCs/records";
+    private static final String POST_URL = "https://open.feishu.cn/open-apis/bitable/v1/apps/I1RlbBcy8aJEjZsIdalckZFdnse/tables/tblIRpAqIbqfqOCs/records/batch_create";
     //    private static final Pattern PATTERN = Pattern.compile("([^#&=]+)=([^#&=]*)");
     private TextView textViewResult;
     private int count = 0;
@@ -65,6 +68,7 @@ public class InboundActivity extends AppCompatActivity {
         totalQuantityTextView = findViewById(R.id.totalquantitytextview);
         barCodeListView = findViewById(R.id.barCodeListView);
         barCodeList = new ArrayList<>();
+        barUrlList = new ArrayList<>();
 
 //        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, barCodeList);
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, barCodeList);
@@ -76,22 +80,21 @@ public class InboundActivity extends AppCompatActivity {
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
                     String barCode = barcodeEditText.getText().toString().trim();
+
 //                    Toast.makeText(InboundActivity.this,parseUrlModel(barCode),Toast.LENGTH_LONG).show();
                     //添加货号到列表
                     if (!barCode.isEmpty()) {
-                        barCodeList.add(parseUrlModel(barCode));
-                        adapter.notifyDataSetChanged();
-                        barcodeEditText.setText("");
-
-                        if (barCodeList.size() >= 3) {
+                        barCodeList.add(parseUrlModel(barCode)); //将扫描内容添加到文本杠list,其和list视图绑定
+                        barUrlList.add(barCode);//添加到内置list，用来上传到飞书
+                        adapter.notifyDataSetChanged(); //触发事件
+                        listCount++; //一次增加一次
+                        barcodeEditText.setText("");//扫描完清空文本框
+                        updateTextViewResult(barCodeList.size()); //更新下面的小字
+                        if (listCount == 5) {    //每两次上传一下，防止万一下子过多时，导致全部白扫
                             // 当行数超过10时，执行发送请求的操作
-//                            Map<String, String> params = extractParams(barCode);
-
-                            // 构建 JSON 数据
-                            String json = buildJson(barCode);
-                            Toast.makeText(InboundActivity.this,json,Toast.LENGTH_LONG).show();
-                            // 发送 POST 请求
-                            sendPostRequest(json);
+                            new UploadDataTask().execute(barUrlList); //提交到飞书
+                            listCount = 0;  //计数器清零重新开始
+//                            barUrlList.clear(); //列表清零
                         }
                     }
                     return true;
@@ -104,90 +107,89 @@ public class InboundActivity extends AppCompatActivity {
 
 
     }
-//添加事件
 
-//    private static Map<String, String> extractParams(String input) {
-//        Map<String, String> paramMap = new HashMap<>();
-//
-//        // 匹配URL中的参数部分
-//        Pattern pattern = Pattern.compile("spm=(.*)&makeday(\\d+)&model(.+)&batch(.*)&sn(.*)"); // 匹配参数名和可能的值
-//        Matcher matcher = pattern.matcher(input);
-//
-//        // 提取参数名和参数值，并放入 paramMap 中
-//        while (matcher.find()) {
-//            String paramName = matcher.group(1);
-//            String paramValue = matcher.group(2) != null ? matcher.group(2) : ""; // 处理无赋值情况
-//            paramMap.put(paramName, paramValue);
-//        }
-//
-//        return paramMap;
-//    }
 
-    private static String buildJson(String url) {
-        // 将url转为json格式
-        Pattern pattern = Pattern.compile("spm=(.*?)&makeday(.*?)&model(.*?)&batch(.*?)&sn(.*)"); // 匹配参数名和可能的值
-        Matcher matcher = pattern.matcher(url);
-        String uniquecode="无";
-        String makeday="无";
-        String model="无";
-        String batch="无";
-        String sn="无";
-        if (matcher.find()){
-            uniquecode=matcher.group(1);
-            makeday=matcher.group(2);
-            model=matcher.group(3);
-            batch=matcher.group(4);
-            sn=matcher.group(5);
-        }
-        return "{\n" +
-                "  \"records\": [\n" +
-                "    {\n" +
-                "      \"fields\": {\n" +
-                "        \"货号\": \"" + model + "\",\n" +
-                "        \"批次\": \"" + batch + "\",\n" +
-                "        \"序列号\": \"" + sn + "\",\n" +
-                "        \"生产日期\": \"" + makeday + "\",\n" +
-                "        \"操作人员\": \"" + user + "\",\n" +
-                "        \"唯一码\": \"" + uniquecode + "\"\n" +
-                "      }\n" +
-                "    }\n" +
-                "  ]\n" +
-                "}";
-    }
+    //新代码
+    private static class UploadDataTask extends AsyncTask<List<String>, Void, Boolean> {
+        public boolean status = true; //异步不能直接处理UI，所有要定义状态
 
-    private static void sendPostRequest(String json) {
-        new PostRequestTask().execute(json);
-    }
-
-    private static class PostRequestTask extends AsyncTask<String, Void, Void> {
         @Override
-        protected Void doInBackground(String... params) {
-            OkHttpClient client = new OkHttpClient();
-            String json = params[0];
-
-            RequestBody requestBody = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), json);
-
-            Request request = new Request.Builder()
-                    .url(POST_URL)
-                    .post(requestBody)
-                    .addHeader("Authorization", feishuAuthor)
-                    .build();
-
+        protected Boolean doInBackground(List<String>... params) {
+            List<String> barCodes = params[0];
             try {
-                Response response = client.newCall(request).execute();
-                if (response.isSuccessful()) {
-//                    System.out.println();
-                } else {
-//                    System.out.println("POST请求失败，响应码：" + response.code());
+                // 构建 JSON 数据
+                JSONArray recordsArray = new JSONArray();
+                for (String barCode : barCodes) {
+                    JSONObject fieldsObject = parseBarCode(barCode);
+                    Log.d("parserA", fieldsObject.toString());
+                    JSONObject recordObject = new JSONObject();
+                    recordObject.put("fields", fieldsObject);
+                    Log.d("parserB", fieldsObject.toString());
+                    recordsArray.put(recordObject);
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
 
-            return null;
+                JSONObject requestBody = new JSONObject();
+                Log.d("parserC", recordsArray.toString());
+                requestBody.put("records", recordsArray);
+                Log.d("parserD", requestBody.toString());
+                // 发送 POST 请求
+                OkHttpClient client = new OkHttpClient();
+                RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), requestBody.toString());
+                Request request = new Request.Builder()
+                        .url(POST_URL)
+                        .addHeader("Authorization", feishuAuthor)
+                        .post(body)
+                        .build();
+
+                Response response = client.newCall(request).execute();
+                Log.d("parserD", recordsArray.toString());
+                return response.isSuccessful() && parseResponse(response.body().string());
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            if (success) {
+                status = true;
+                barUrlList.clear();
+            } else {
+                status = false;
+            }
+        }
+
+        //解析条码
+        protected JSONObject parseBarCode(String barCode) throws JSONException {
+            //提取URL里面的字段
+            Pattern pattern = Pattern.compile("spm=(.*)&makeday(.*)&model(.+)&batch(.*)&sn(.*)"); // 匹配参数名和可能的值
+            Matcher matcher = pattern.matcher(barCode);
+            String uniquecode = null, model = null, makeday = null, batch = null, sn = null;
+            //正则表达式
+            if (matcher.find()) {
+                uniquecode = matcher.group(1);
+                makeday = matcher.group(2);
+                model = matcher.group(3);
+                batch = matcher.group(4);
+                sn = matcher.group(5);
+            }
+            JSONObject fieldsObject = new JSONObject();
+            fieldsObject.put("货号", model);
+            fieldsObject.put("批次", batch);
+            fieldsObject.put("序列号", sn);
+            fieldsObject.put("生产日期", makeday);
+            fieldsObject.put("唯一码", uniquecode);
+            fieldsObject.put("操作人员", operator);
+            return fieldsObject;
+        }
+
+        //解析返回结果
+        private boolean parseResponse(String response) throws JSONException {
+            JSONObject jsonResponse = new JSONObject(response);
+            return jsonResponse.optInt("code") == 0;
         }
     }
-
 
     private void playNotificationSound() {
         // 播放提示音，这里使用默认提示音
@@ -195,16 +197,15 @@ public class InboundActivity extends AppCompatActivity {
         mediaPlayer.start();
     }
 
-    private void updateTextViewResult() {
+    private void updateTextViewResult(Integer count) {
         // 更新底部的 TextView 中的数字
-        count++;
         currentQuantityTextView.setText("当前数量: " + count);
     }
 
-//    http://www.xpbiomed.com/productinfo/1396386.html#C3580-0500-2349389-05122025#spm=Ta760724US&makeday1701423233&modelC3580-0500&batch2349389&snXP1999
+
     private static String parseUrlModel(String url) {
 //        Pattern pattern=Pattern.compile("model(\\w.+)&batch");
-        return  url.substring(115,125);
+        return url.substring(115, 125);
 
 
     }
